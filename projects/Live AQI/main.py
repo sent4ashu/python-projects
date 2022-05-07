0b91fc3a06769a2cf430f5d657072e94243ba701
Ashutosh <sent4ashu@gmail.com>
2022-05-07 10:00:00 -0400

    update

--- projects/Live AQI/main.py
+++ projects/Live AQI/main.py
@@ -21,7 +21,7 @@ city_entry.pack()
 state_label = tk.Label(app, text="State:")
 state_label.pack() 
 state_label.configure(bg="#F0F0F0")  # Set the background color of the label
-state_entry = tk.Entry(app)
+state_entry = tk.Entry(app) 
 state_entry.pack() 
 
 country_label = tk.Label(app, text="Country:")
